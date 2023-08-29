0d9786cc862486c066917a6b90834e799d803f05
Amir <Amirparadox47@gmail.com>
2023-08-29 17:20:49 +0330

    part #8

--- assets/js/alpinejControllers/usersController.js
+++ assets/js/alpinejControllers/usersController.js
@@ -1,18 +1,41 @@
 document.addEventListener('alpine:init', () => {
-    Alpine.data('usersData', function(){
+    Alpine.data('usersData', ()=>({
+        users: [],
+        pageUsers: [],
+        isloading: false,
+        showAddModal: false,
+        pageCount: 1,
+        itemsCount: 4,
+        currentPage: 1,
+        getUsers(){
+            this.isloading = true 
+            axios.get("https://jsonplaceholder.typicode.com/users").then((res)=>{
+                this.users = res.data
+                this.pagination()
 
-        return{
-            users: [],
-            isloading: false,
-            getUsers(){
-                this.isloading = true 
-                axios.get("https://jsonplaceholder.typicode.com/users").then((res)=>{
-                    this.users = res.data
-                    this.isloading = false
-                })
-            }
-        }
+            }).finally(()=>{
+                this.isloading = false
+
+            })
+        },
+        pagination(){
+            this.pageCount = Math.ceil(this.users.length / this.itemsCount) // 10 / 4 = 3
+            const end = this.currentPage * this.itemsCount // 4
+            const start = (this.currentPage * this.itemsCount) - this.itemsCount // 0
+            this.pageUsers = this.users.slice(start , end)
+            console.log(this.pageUsers)
+        },
         
-    })
+        nextPage(){
+            this.currentPage++
+            if(this.currentPage > this.pageCount) this.currentPage = this.pageCount
+            this.pagination()
+        },
     
+        prevPage(){
+            this.currentPage--
+            if(this.currentPage < 1) this.currentPage = 1
+            this.pagination()
+        }
+    }))
 })
